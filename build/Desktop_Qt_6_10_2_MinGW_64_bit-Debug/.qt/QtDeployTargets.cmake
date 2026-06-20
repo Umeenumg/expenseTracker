ae85967ee702fddaf1016a5fe220871a75cad44a
Imane Megouar <megouarimane@gmail.com>
2026-06-20 16:08:33 +0100

    Remove build artifacts, add .gitignore

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ExpensesTracker_FILE C:/Users/hp/Desktop/ExpensesTracker/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/ExpensesTracker.exe)
-set(__QT_DEPLOY_TARGET_ExpensesTracker_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ExpensesTracker_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
