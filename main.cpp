e3e1a4a18cfdc67219c7fd4b0354f29ed39f369a
Imane Megouar <megouarimane@gmail.com>
2026-06-23 11:15:30 +0100

    Apply unified global stylesheet and fix dialog layouts

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
+#include "ui/LoginWindow.h"
+#include "database/DatabaseManager.h"
 #include <QApplication>
 #include <QFile>
-#include "database/DatabaseManager.h"
-#include "ui/LoginWindow.h"
+#include <QDebug>
 
 QSqlDatabase g_db;
 DatabaseManager *g_dbManager = nullptr;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
 
     QFile styleFile(":/style.qss");
-    if (styleFile.open(QFile::ReadOnly)) {
-        a.setStyleSheet(QString::fromUtf8(styleFile.readAll()));
-        styleFile.close();
-    }
+    qDebug() << "File exists:" << styleFile.exists();
 
+ /*   if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        QString styleSheet = QLatin1String(styleFile.readAll());
+        qDebug() << "Stylesheet length:" << styleSheet.length();
+        app.setStyleSheet(styleSheet);
+        styleFile.close();
+    } else {
+        qDebug() << "FAILED to open style.qss!";
+    }
+*/
     DatabaseManager dbManager;
     g_dbManager = &dbManager;
-
     if (!dbManager.openDatabase()) {
         return -1;
     }
-
     g_db = dbManager.getDatabase();
 
     LoginWindow login;
     login.show();
-
-    return a.exec();
+    return app.exec();
 }
